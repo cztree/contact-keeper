1688735b7c79990646109fb9413110b73a4e2d30
tree <lightninghug7@gmail.com>
2019-09-29 15:06:09 +0300

    ContactForm Component

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -42,6 +42,10 @@ const ContactState = props => {
   const [state, dispatch] = useReducer(contactReducer, initialState)
 
   // Add Contact
+  const addContact = contact => {
+    contact.id = uuid.v4()
+    dispatch({ type: ADD_CONTACT, payload: contact })
+  }
 
   // Delete Contact
 
@@ -57,10 +61,12 @@ const ContactState = props => {
 
   return (
     <ContactContext.Provider
-     value={{
-       contacts: state.contacts
-     }}>
-      { props.children }
+      value={{
+        contacts: state.contacts,
+        addContact
+      }}
+    >
+      {props.children}
     </ContactContext.Provider>
   )
 }
