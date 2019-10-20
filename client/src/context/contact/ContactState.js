7b7a6a728af7b1feca815ad5f82e37a6156d8798
tree <lightninghug7@gmail.com>
2019-10-20 11:53:13 +0300

    Edit - Update contact action

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -58,6 +58,7 @@ const ContactState = props => {
   const clearCurrent = () => dispatch({ type: CLEAR_CURRENT })
 
   // Update Contact
+  const updateContact = contact => dispatch({ type: UPDATE_CONTACT, payload: contact })
 
   // Filter Contacts
 
@@ -71,7 +72,8 @@ const ContactState = props => {
         addContact,
         deleteContact,
         setCurrent,
-        clearCurrent
+        clearCurrent,
+        updateContact
       }}
     >
       {props.children}
